c8a164a28079c598aa6dfdec98f4425f5691943e
leokang123 <leokang123@github.com>
2025-07-09 10:46:21 +0900

    feat: 함수 이름 변경 - 각 페이지에서 title넣는 란 삭제

--- lib/view/login.dart
+++ lib/view/login.dart
@@ -91,8 +91,7 @@ class _LoginScreenState extends State<LoginScreen> {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) =>
-                        const RegisterEmailPageScreen(title: "Login"),
+                    builder: (context) => const RegisterEmailPageScreen(),
                   ),
                 );
               },
