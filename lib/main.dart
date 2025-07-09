31590c876ebe0dca63fbe0b1ec84753414c3bbe0
leokang123 <leokang123@github.com>
2025-07-09 16:22:13 +0900

    refactor: 불필요한 임포트 지우기

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:yoen_front/view/login.dart';
-import 'package:yoen_front/view/register_profile_url.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
