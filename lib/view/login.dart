a856d577b70aa020f1a129588860fe875e759fe6
leokang123 <leokang123@github.com>
2025-07-09 17:06:45 +0900

    feat: 로그인창에 FocusNode 추가하여 편의기능 추가

--- lib/view/login.dart
+++ lib/view/login.dart
@@ -13,111 +13,157 @@ class LoginScreen extends ConsumerStatefulWidget {
 
 class _LoginScreenState extends ConsumerState<LoginScreen> {
   bool isObscuredPwd = true;
-  TextEditingController emailController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final FocusNode emailFocusNode = FocusNode();
+  final FocusNode passwordFocusNode = FocusNode();
 
   @override
   void dispose() {
     emailController.dispose();
     passwordController.dispose();
+    emailFocusNode.dispose();
+    passwordFocusNode.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(40.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const Spacer(flex: 2),
-            const Text(
-              'Yoen',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
-            ),
-            const Spacer(flex: 1),
-            TextField(
-              controller: emailController,
-              decoration: InputDecoration(
-                hintText: 'Email',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Spacer(flex: 2),
+              const Text(
+                'Yoen',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
               ),
-            ),
-            const SizedBox(height: 16),
-            TextField(
-              controller: passwordController,
-              obscureText: isObscuredPwd,
-              decoration: InputDecoration(
-                labelText: 'Password',
-                hintText: isObscuredPwd ? '*******' : "Example1!",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                suffixIcon: IconButton(
-                  icon: Icon(
-                    isObscuredPwd ? Icons.visibility_off : Icons.visibility,
-                    color: Theme.of(context).primaryColorDark,
+              const Spacer(flex: 1),
+              TextField(
+                controller: emailController,
+                focusNode: emailFocusNode,
+                onSubmitted: (_) {
+                  FocusScope.of(
+                    context,
+                  ).requestFocus(passwordFocusNode); // 다음으로 포커스 이동
+                },
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
                   ),
-                  onPressed: () {
-                    setState(() {
-                      isObscuredPwd = !isObscuredPwd;
-                    });
-                  },
                 ),
               ),
-            ),
-            const SizedBox(height: 24),
-            ElevatedButton(
-              onPressed: () async {
-                final loginNotifier = ref.read(loginNotifierProvider.notifier);
-                final navigator = Navigator.of(context);
-                final messenger = ScaffoldMessenger.of(context);
-                await loginNotifier.login(
-                  emailController.text.trim(),
-                  passwordController.text.trim(),
-                );
-                final state = ref.read(loginNotifierProvider);
-                if (state.status == LoginStatus.success) {
-                  // 성공 시 페이지 이동
-                  navigator.push(
-                    MaterialPageRoute(builder: (context) => const BaseScreen()),
+              const SizedBox(height: 16),
+              TextField(
+                controller: passwordController,
+                focusNode: passwordFocusNode,
+                onSubmitted: (_) async {
+                  final loginNotifier = ref.read(
+                    loginNotifierProvider.notifier,
                   );
-                } else if (state.status == LoginStatus.error) {
-                  // 실패 시 에러 메시지 표시
-                  messenger.showSnackBar(
-                    SnackBar(content: Text(state.errorMessage ?? "로그인 실패")),
+                  final navigator = Navigator.of(context);
+                  final messenger = ScaffoldMessenger.of(context);
+                  await loginNotifier.login(
+                    emailController.text.trim(),
+                    passwordController.text.trim(),
                   );
-                }
-              },
-              style: ElevatedButton.styleFrom(
-                padding: const EdgeInsets.symmetric(vertical: 16),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-              ),
-              child: const Text('Login', style: TextStyle(fontSize: 18)),
-            ),
-            const Spacer(flex: 2),
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const RegisterEmailPageScreen(),
+                  final state = ref.read(loginNotifierProvider);
+                  if (state.status == LoginStatus.success) {
+                    // 성공 시 페이지 이동
+                    navigator.pushAndRemoveUntil(
+                      MaterialPageRoute(
+                        builder: (context) => const BaseScreen(),
+                      ),
+                      (route) => false,
+                    );
+                  } else if (state.status == LoginStatus.error) {
+                    // 실패 시 에러 메시지 표시
+                    messenger.showSnackBar(
+                      SnackBar(content: Text(state.errorMessage ?? "로그인 실패")),
+                    );
+                  }
+                },
+                obscureText: isObscuredPwd,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  hintText: isObscuredPwd ? '*******' : "Example1!",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
                   ),
-                );
-              },
-              child: const Text(
-                '회원가입',
-                style: TextStyle(color: Colors.grey, fontSize: 16),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      isObscuredPwd ? Icons.visibility_off : Icons.visibility,
+                      color: Theme.of(context).primaryColorDark,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        isObscuredPwd = !isObscuredPwd;
+                      });
+                    },
+                  ),
+                ),
               ),
-            ),
-          ],
+              const SizedBox(height: 24),
+              ElevatedButton(
+                onPressed: () async {
+                  final loginNotifier = ref.read(
+                    loginNotifierProvider.notifier,
+                  );
+                  final navigator = Navigator.of(context);
+                  final messenger = ScaffoldMessenger.of(context);
+                  await loginNotifier.login(
+                    emailController.text.trim(),
+                    passwordController.text.trim(),
+                  );
+                  final state = ref.read(loginNotifierProvider);
+                  if (state.status == LoginStatus.success) {
+                    // 성공 시 페이지 이동
+                    navigator.pushAndRemoveUntil(
+                      MaterialPageRoute(
+                        builder: (context) => const BaseScreen(),
+                      ),
+                      (route) => false,
+                    );
+                  } else if (state.status == LoginStatus.error) {
+                    // 실패 시 에러 메시지 표시
+                    messenger.showSnackBar(
+                      SnackBar(content: Text(state.errorMessage ?? "로그인 실패")),
+                    );
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                child: const Text('Login', style: TextStyle(fontSize: 18)),
+              ),
+              const Spacer(flex: 2),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const RegisterEmailPageScreen(),
+                    ),
+                  );
+                },
+                child: const Text(
+                  '회원가입',
+                  style: TextStyle(color: Colors.grey, fontSize: 16),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
