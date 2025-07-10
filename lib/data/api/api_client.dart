13fea99989a80f2edf2facf98c772e4d2540a515
leokang123 <leokang123@github.com>
2025-07-10 14:22:34 +0900

    feat: 토큰 적용 기능 구현 - 액세스 토큰 헤더안에 - 리프레시 토큰으로 재요청

--- lib/data/api/api_client.dart
+++ lib/data/api/api_client.dart
@@ -1,14 +1,35 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'api_service.dart';
+import 'interceptor/auth_interceptor.dart';
 
 class ApiClient {
-  static final Dio _dio = Dio(
-    BaseOptions(
-      connectTimeout: const Duration(seconds: 10),
-      receiveTimeout: const Duration(seconds: 10),
-      headers: {'Content-Type': 'application/json'},
-    ),
-  );
+  static Dio createDio(Ref ref) {
+    final dio = Dio(
+      BaseOptions(
+        connectTimeout: const Duration(seconds: 10),
+        receiveTimeout: const Duration(seconds: 10),
+        headers: {'Content-Type': 'application/json'},
+      ),
+    );
 
-  static ApiService get apiService => ApiService(_dio);
+    dio.interceptors.add(AuthInterceptor(ref, dio));
+    dio.interceptors.add(
+      LogInterceptor(
+        request: true,
+        requestHeader: true,
+        requestBody: true,
+        responseHeader: true,
+        responseBody: true,
+        error: true,
+        logPrint: print, // 로그 출력 방식, 기본은 print
+      ),
+    );
+
+    return dio;
+  }
+
+  static ApiService createApiService(Ref ref) {
+    return ApiService(createDio(ref));
+  }
 }
---
+++ lib/data/api/interceptor/auth_interceptor.dart
@@ -0,0 +1,79 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../notifier/login_notifier.dart';
+
+class AuthInterceptor extends Interceptor {
+  final Ref ref;
+  final Dio dio;
+
+  AuthInterceptor(this.ref, this.dio);
+
+  @override
+  void onRequest(
+    RequestOptions options,
+    RequestInterceptorHandler handler,
+  ) async {
+    final storage = ref.read(secureStorageProvider);
+    final accessToken = await storage.read(key: 'accessToken');
+
+    if (accessToken != null) {
+      options.headers['Authorization'] = 'Bearer $accessToken';
+    }
+
+    handler.next(options);
+  }
+
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) async {
+    // access token이 만료되었을 경우
+    if (err.response?.statusCode == 401 && !_isRetry(err.requestOptions)) {
+      final storage = ref.read(secureStorageProvider);
+      final refreshToken = await storage.read(key: 'refreshToken');
+
+      if (refreshToken != null) {
+        try {
+          final response = await dio.post(
+            '/auth/refresh',
+            data: {'refreshToken': refreshToken},
+          );
+
+          final newAccessToken = response.data['accessToken'];
+          final newRefreshToken = response.data['refreshToken'];
+
+          await storage.write(key: 'accessToken', value: newAccessToken);
+          await storage.write(key: 'refreshToken', value: newRefreshToken);
+
+          final requestOptions = err.requestOptions;
+
+          // 재시도 플래그 설정
+          requestOptions.extra['retried'] = true;
+
+          requestOptions.headers['Authorization'] = 'Bearer $newAccessToken';
+
+          final clonedResponse = await dio.request(
+            requestOptions.path,
+            data: requestOptions.data,
+            queryParameters: requestOptions.queryParameters,
+            options: Options(
+              method: requestOptions.method,
+              headers: requestOptions.headers,
+            ),
+          );
+
+          return handler.resolve(clonedResponse);
+        } catch (_) {
+          await storage.deleteAll();
+          return handler.reject(err); // 재발급 실패 → 그대로 에러 처리
+        }
+      }
+    }
+
+    return handler.next(err);
+  }
+
+  bool _isRetry(RequestOptions options) {
+    return options.extra['retried'] == true;
+  }
+}
