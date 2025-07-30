5d872c9af7206cd1612f2d036d4f238d44e821c5
leokang123 <leokang123@github.com>
2025-07-30 13:04:16 +0900

    feat: user travel join dto 생성

---
+++ lib/data/model/user_travel_join_response.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_travel_join_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+UserTravelJoinResponse _$UserTravelJoinResponseFromJson(
+  Map<String, dynamic> json,
+) => UserTravelJoinResponse(
+  travelId: (json['travelId'] as num).toInt(),
+  travelName: json['travelName'] as String,
+  nation: json['nation'] as String,
+  users: (json['users'] as List<dynamic>)
+      .map((e) => UserResponse.fromJson(e as Map<String, dynamic>))
+      .toList(),
+);
+
+Map<String, dynamic> _$UserTravelJoinResponseToJson(
+  UserTravelJoinResponse instance,
+) => <String, dynamic>{
+  'travelId': instance.travelId,
+  'travelName': instance.travelName,
+  'nation': instance.nation,
+  'users': instance.users,
+};
