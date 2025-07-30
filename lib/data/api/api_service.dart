979c77dd037a7f21628d36e908674a1854438c5d
leokang123 <leokang123@github.com>
2025-07-30 17:10:54 +0900

    feat: 참여신청한 여행 리스트 출력 기능 구현

--- lib/data/api/api_service.dart
+++ lib/data/api/api_service.dart
@@ -4,6 +4,7 @@ import 'package:yoen_front/data/model/api_response.dart';
 import 'package:yoen_front/data/model/destination_response.dart';
 import 'package:yoen_front/data/model/travel_create_request.dart';
 import 'package:yoen_front/data/model/travel_create_response.dart';
+import 'package:yoen_front/data/model/user_travel_join_response.dart';
 import '../model/login_request.dart';
 import '../model/login_response.dart';
 import '../model/register_request.dart';
@@ -25,9 +26,24 @@ abstract class ApiService {
 
   @GET("/common/destination/all")
   Future<ApiResponse<List<DestinationResponse>>> getDestinations(
-      @Query("nation") String nation);
+    @Query("nation") String nation,
+  );
 
   @POST("/travel/create")
   Future<ApiResponse<TravelCreateResponse>> createTravel(
-      @Body() TravelCreateRequest request);
+    @Body() TravelCreateRequest request,
+  );
+
+  @GET("/join/userlist")
+  Future<ApiResponse<List<UserTravelJoinResponse>>> getUserJoinList();
+
+  @POST("/join/{joinCode}")
+  Future<ApiResponse<String>> joinTravelByCode(
+    @Path("joinCode") String joinCode,
+  );
+
+  @DELETE("/join/delete/{id}")
+  Future<ApiResponse<String>> deleteUserJoinTravel(
+    @Path("id") int travelJoinId,
+  );
 }
--- lib/data/api/api_service.g.dart
+++ lib/data/api/api_service.g.dart
@@ -181,6 +181,104 @@ class _ApiService implements ApiService {
     return _value;
   }
 
+  @override
+  Future<ApiResponse<List<UserTravelJoinResponse>>> getUserJoinList() async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<ApiResponse<List<UserTravelJoinResponse>>>(
+      Options(method: 'GET', headers: _headers, extra: _extra)
+          .compose(
+            _dio.options,
+            '/join/userlist',
+            queryParameters: queryParameters,
+            data: _data,
+          )
+          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+    );
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late ApiResponse<List<UserTravelJoinResponse>> _value;
+    try {
+      _value = ApiResponse<List<UserTravelJoinResponse>>.fromJson(
+        _result.data!,
+        (json) => json is List<dynamic>
+            ? json
+                  .map<UserTravelJoinResponse>(
+                    (i) => UserTravelJoinResponse.fromJson(
+                      i as Map<String, dynamic>,
+                    ),
+                  )
+                  .toList()
+            : List.empty(),
+      );
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
+  @override
+  Future<ApiResponse<String>> joinTravelByCode(String joinCode) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<ApiResponse<String>>(
+      Options(method: 'POST', headers: _headers, extra: _extra)
+          .compose(
+            _dio.options,
+            '/join/${joinCode}',
+            queryParameters: queryParameters,
+            data: _data,
+          )
+          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+    );
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late ApiResponse<String> _value;
+    try {
+      _value = ApiResponse<String>.fromJson(
+        _result.data!,
+        (json) => json as String,
+      );
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
+  @override
+  Future<ApiResponse<String>> deleteUserJoinTravel(int travelJoinId) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<ApiResponse<String>>(
+      Options(method: 'DELETE', headers: _headers, extra: _extra)
+          .compose(
+            _dio.options,
+            '/join/delete/${travelJoinId}',
+            queryParameters: queryParameters,
+            data: _data,
+          )
+          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+    );
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late ApiResponse<String> _value;
+    try {
+      _value = ApiResponse<String>.fromJson(
+        _result.data!,
+        (json) => json as String,
+      );
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
