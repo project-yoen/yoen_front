86a9046be2a5e19afe027cb3452d6da9cc2c4a9c
leokang123 <leokang123@github.com>
2025-07-31 15:26:59 +0900

    feat: 여행부문 조인요청자 구현 함수 구현

---
+++ lib/data/notifier/travel_join_notifier.dart
@@ -0,0 +1,118 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:yoen_front/data/model/accept_join_request.dart';
+import 'package:yoen_front/data/model/join_code_response.dart';
+import 'package:yoen_front/data/model/travel_user_join_response.dart';
+import 'package:yoen_front/data/repository/join_repository.dart';
+
+final travelJoinNotifierProvider =
+    StateNotifierProvider<TravelJoinNotifier, TravelJoinState>((ref) {
+      final repository = ref.watch(joinRepositoryProvider);
+      return TravelJoinNotifier(repository);
+    });
+
+class TravelJoinNotifier extends StateNotifier<TravelJoinState> {
+  final JoinRepository _repository;
+
+  TravelJoinNotifier(this._repository) : super(TravelJoinState.initial());
+
+  Future<void> getTravelJoinList(int travelId) async {
+    state = state.copyWith(status: TravelJoinStatus.loading);
+    try {
+      final response = await _repository.getTravelJoinList(travelId);
+      state = state.copyWith(
+        status: TravelJoinStatus.success,
+        userJoins: response,
+      );
+    } catch (e) {
+      state = state.copyWith(
+        status: TravelJoinStatus.error,
+        errorMessage: e.toString(),
+      );
+    }
+  }
+
+  Future<void> getTravelCode(int travelId) async {
+    state = state.copyWith(status: TravelJoinStatus.loading);
+    try {
+      final response = await _repository.getJoinCode(travelId);
+      state = state.copyWith(
+        status: TravelJoinStatus.success,
+        joinCode: response,
+      );
+    } catch (e) {
+      state = state.copyWith(
+        status: TravelJoinStatus.error,
+        errorMessage: e.toString(),
+      );
+    }
+  }
+
+  Future<void> acceptTravelJoin(int travelJoinId, String role) async {
+    state = state.copyWith(status: TravelJoinStatus.loading);
+    try {
+      AcceptJoinRequest request = AcceptJoinRequest(
+        travelJoinRequestId: travelJoinId,
+        role: role,
+      );
+      final response = await _repository.acceptTravelJoin(request);
+      state = state.copyWith(status: TravelJoinStatus.success);
+      // await getTravelJoinList();
+    } catch (e) {
+      state = state.copyWith(
+        status: TravelJoinStatus.error,
+        errorMessage: e.toString(),
+      );
+    }
+  }
+
+  Future<void> rejectTravelJoin(int travelJoinId) async {
+    state = state.copyWith(status: TravelJoinStatus.loading);
+    try {
+      final response = await _repository.rejectTravelJoin(travelJoinId);
+      state = state.copyWith(status: TravelJoinStatus.success);
+    } catch (e) {
+      state = state.copyWith(
+        status: TravelJoinStatus.error,
+        errorMessage: e.toString(),
+      );
+    }
+  }
+
+  void reset() {
+    state = TravelJoinState.initial();
+  }
+}
+
+enum TravelJoinStatus { initial, loading, success, error }
+
+class TravelJoinState {
+  final TravelJoinStatus status;
+  final List<TravelUserJoinResponse> userJoins;
+  final JoinCodeResponse? joinCode;
+  final String? errorMessage;
+
+  TravelJoinState({
+    this.status = TravelJoinStatus.initial,
+    this.userJoins = const [],
+    this.errorMessage,
+    this.joinCode,
+  });
+
+  factory TravelJoinState.initial() {
+    return TravelJoinState(status: TravelJoinStatus.initial);
+  }
+
+  TravelJoinState copyWith({
+    TravelJoinStatus? status,
+    List<TravelUserJoinResponse>? userJoins,
+    JoinCodeResponse? joinCode,
+    String? errorMessage,
+  }) {
+    return TravelJoinState(
+      status: status ?? this.status,
+      userJoins: userJoins ?? this.userJoins,
+      joinCode: joinCode ?? this.joinCode,
+      errorMessage: errorMessage ?? this.errorMessage,
+    );
+  }
+}
--- lib/data/repository/join_repository.dart
+++ lib/data/repository/join_repository.dart
@@ -1,5 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:yoen_front/data/api/api_service.dart';
+import 'package:yoen_front/data/model/accept_join_request.dart';
+import 'package:yoen_front/data/model/join_code_response.dart';
+import 'package:yoen_front/data/model/travel_user_join_response.dart';
 import 'package:yoen_front/data/model/user_travel_join_response.dart';
 
 import '../api/api_provider.dart';
@@ -19,7 +22,32 @@ class JoinRepository {
     if (apiResponse.data != null) {
       return apiResponse.data!;
     } else {
-      throw Exception(apiResponse.error ?? 'Failed to create join');
+      throw Exception(apiResponse.error ?? 'Failed to print join');
+    }
+  }
+
+  Future<List<TravelUserJoinResponse>> getTravelJoinList(int travelId) async {
+    final apiResponse = await _apiService.getTravelJoinList(travelId);
+    if (apiResponse.data != null) {
+      return apiResponse.data!;
+    } else {
+      throw Exception(apiResponse.error ?? 'Failed to print join');
+    }
+  }
+
+  Future<void> acceptTravelJoin(AcceptJoinRequest request) async {
+    try {
+      await _apiService.acceptTravelJoinRequest(request);
+    } catch (_) {
+      rethrow;
+    }
+  }
+
+  Future<void> rejectTravelJoin(int travelJoinId) async {
+    try {
+      await _apiService.rejectTravelJoinRequest(travelJoinId);
+    } catch (_) {
+      rethrow;
     }
   }
 
@@ -36,6 +64,15 @@ class JoinRepository {
       return apiResponse.data!;
     } else {
       throw Exception(apiResponse.error ?? 'Failed to create join');
+    }
+  }
+
+  Future<JoinCodeResponse> getJoinCode(int travelId) async {
+    final apiResponse = await _apiService.getJoinCode(travelId);
+    if (apiResponse.success == true) {
+      return apiResponse.data!;
+    } else {
+      throw Exception(apiResponse.error ?? 'Failed to get joinCode');
     }
   }
 }
