3368d0d3c51878481f81dc28aae4dec0d5218ded
leokang123 <leokang123@github.com>
2025-08-04 15:33:31 +0900

    feat: routeObserver, ScaffoldMessengerState main.dart에 선언
    - base화면에서 pop을 대기할수있고, snackbar를 context없이 호출할수 있음

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:yoen_front/view/base.dart';
+import 'package:yoen_front/view/login.dart';
 import 'package:yoen_front/view/splash.dart';
 
 void main() async {
@@ -13,16 +15,45 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      scaffoldMessengerKey: snackbarKey,
+      navigatorObservers: [routeObserver],
       debugShowCheckedModeBanner: false,
       title: 'Yoen',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      // home: const LoginScreen(),
-      home: const SplashScreen(),
+      initialRoute: '/',
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case '/':
+            return MaterialPageRoute(
+              builder: (_) => const SplashScreen(),
+              settings: settings,
+            );
+          case '/login':
+            return MaterialPageRoute(
+              builder: (_) => const LoginScreen(),
+              settings: settings,
+            );
+          case '/base':
+            return MaterialPageRoute(
+              builder: (_) => const BaseScreen(),
+              settings: settings,
+            );
+          default:
+            return null;
+        }
+      },
     );
   }
 }
+
+final RouteObserver<ModalRoute<void>> routeObserver =
+    RouteObserver<ModalRoute<void>>();
+
+final GlobalKey<ScaffoldMessengerState> snackbarKey =
+    GlobalKey<ScaffoldMessengerState>();
