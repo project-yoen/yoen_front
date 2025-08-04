58fc978de246372654e4954bef5921d43bf72b61
Seojun <auddml321@hanyang.ac.kr>
2025-08-04 13:54:49 +0900

    feat: 금액기록 생성을 눌렀을때 3개의 옵션이 표시되게 변경

--- lib/view/travel_payment.dart
+++ lib/view/travel_payment.dart
@@ -3,6 +3,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:yoen_front/data/notifier/date_notifier.dart';
 import 'package:yoen_front/data/notifier/travel_list_notifier.dart';
 import 'package:yoen_front/view/travel_payment_create.dart';
+import 'package:yoen_front/view/travel_prepayment_create.dart';
+import 'package:yoen_front/view/travel_sharedfund_create.dart';
 
 class TravelPaymentScreen extends ConsumerWidget {
   const TravelPaymentScreen({super.key});
@@ -19,15 +21,62 @@ class TravelPaymentScreen extends ConsumerWidget {
     return Scaffold(
       body: Center(child: Text('금액 기록 페이지: ${travel.travelId}')),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) => const TravelPaymentCreateScreen(),
-            ),
-          );
-        },
+        onPressed: () => _showPaymentOptions(context),
         child: const Icon(Icons.add),
       ),
+    );
+  }
+
+  void _showPaymentOptions(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Wrap(
+          children: <Widget>[
+            ListTile(
+              leading: const Icon(Icons.group_add),
+              title: const Text('공금기록'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const TravelSharedfundCreateScreen(
+                      paymentType: "SHAREDFUND",
+                    ),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.payment),
+              title: const Text('결제기록'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        const TravelPaymentCreateScreen(paymentType: "PAYMENT"),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.receipt),
+              title: const Text('사전사용금액기록'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const TravelPrepaymentCreateScreen(
+                      paymentType: "PREPAYMENT",
+                    ),
+                  ),
+                );
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
