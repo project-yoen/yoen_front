21c7ff7352a4768c503286fe5eb2bd69998688cf
Seojun <auddml321@hanyang.ac.kr>
2025-08-06 17:09:05 +0900

    feat: payment 페이지에서 카드 눌렀을 때 dialog로 출력되게 변경

--- lib/data/model/payment_create_request.g.dart
+++ lib/data/model/payment_create_request.g.dart
@@ -6,46 +6,47 @@ part of 'payment_create_request.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-PaymentCreateRequest _$PaymentCreateRequestFromJson(Map<String, dynamic> json) =>
-    PaymentCreateRequest(
-      travelId: (json['travelId'] as num).toInt(),
-      travelUserId: (json['travelUserId'] as num?)?.toInt(),
-      paymentName: json['paymentName'] as String,
-      categoryId: (json['categoryId'] as num?)?.toInt(),
-      payerType: json['payerType'] as String,
-      payTime: json['payTime'] as String,
-      paymentMethod: json['paymentMethod'] as String,
-      paymentType: json['paymentType'] as String,
-      paymentAccount: (json['paymentAccount'] as num).toInt(),
-      settlementList: (json['settlementList'] as List<dynamic>)
-          .map((e) => Settlement.fromJson(e as Map<String, dynamic>))
-          .toList(),
-    );
+PaymentCreateRequest _$PaymentCreateRequestFromJson(
+  Map<String, dynamic> json,
+) => PaymentCreateRequest(
+  travelId: (json['travelId'] as num).toInt(),
+  travelUserId: (json['travelUserId'] as num?)?.toInt(),
+  paymentName: json['paymentName'] as String,
+  categoryId: (json['categoryId'] as num?)?.toInt(),
+  payerType: json['payerType'] as String,
+  payTime: json['payTime'] as String,
+  paymentMethod: json['paymentMethod'] as String,
+  paymentType: json['paymentType'] as String,
+  paymentAccount: (json['paymentAccount'] as num).toInt(),
+  settlementList: (json['settlementList'] as List<dynamic>)
+      .map((e) => Settlement.fromJson(e as Map<String, dynamic>))
+      .toList(),
+);
 
 Map<String, dynamic> _$PaymentCreateRequestToJson(
-        PaymentCreateRequest instance) =>
-    <String, dynamic>{
-      'travelId': instance.travelId,
-      'travelUserId': instance.travelUserId,
-      'categoryId': instance.categoryId,
-      'payerType': instance.payerType,
-      'payTime': instance.payTime,
-      'paymentMethod': instance.paymentMethod,
-      'paymentName': instance.paymentName,
-      'paymentType': instance.paymentType,
-      'paymentAccount': instance.paymentAccount,
-      'settlementList': instance.settlementList,
-    };
+  PaymentCreateRequest instance,
+) => <String, dynamic>{
+  'travelId': instance.travelId,
+  'travelUserId': instance.travelUserId,
+  'categoryId': instance.categoryId,
+  'payerType': instance.payerType,
+  'payTime': instance.payTime,
+  'paymentMethod': instance.paymentMethod,
+  'paymentName': instance.paymentName,
+  'paymentType': instance.paymentType,
+  'paymentAccount': instance.paymentAccount,
+  'settlementList': instance.settlementList,
+};
 
 Settlement _$SettlementFromJson(Map<String, dynamic> json) => Settlement(
-      paymentId: (json['paymentId'] as num?)?.toInt(),
-      settlementName: json['settlementName'] as String,
-      amount: (json['amount'] as num).toInt(),
-      isPaid: json['isPaid'] as bool,
-      travelUsers: (json['travelUsers'] as List<dynamic>)
-          .map((e) => (e as num).toInt())
-          .toList(),
-    );
+  paymentId: (json['paymentId'] as num?)?.toInt(),
+  settlementName: json['settlementName'] as String,
+  amount: (json['amount'] as num).toInt(),
+  isPaid: json['isPaid'] as bool,
+  travelUsers: (json['travelUsers'] as List<dynamic>)
+      .map((e) => (e as num).toInt())
+      .toList(),
+);
 
 Map<String, dynamic> _$SettlementToJson(Settlement instance) =>
     <String, dynamic>{
